46db9e3830e2a07f7d348d0773770fdf14252643
yuki akiyama <you2197901@gmail.com>
2019-07-08 15:23:36 +0900

    require -> import

--- src/lib/crypto/hash.ts
+++ src/lib/crypto/hash.ts
@@ -1,14 +1,14 @@
-const createHash = require('create-hash')
+import createHash from 'create-hash'
 
 export const ripemd160 = (buffer: Buffer): Buffer => {
-    return createHash('rmd160').update(buffer).digest()
+  return createHash('rmd160').update(buffer).digest()
 }
 
 export const sha256 = (buffer: Buffer): Buffer => {
-    return createHash('sha256').update(buffer).digest()
+  return createHash('sha256').update(buffer).digest()
 }
 
 export const hash160 = (buffer: Buffer): Buffer => {
-    return ripemd160(sha256(buffer))
+  return ripemd160(sha256(buffer))
 }
 
