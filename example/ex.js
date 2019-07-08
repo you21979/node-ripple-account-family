b660817303db04630fb3f82b0488c25737cb1274
yuki akiyama <you2197901@gmail.com>
2019-07-08 20:33:17 +0900

    update example

--- example/ex.js
+++ example/ex.js
@@ -1,32 +1,19 @@
-const addressCodec = require("ripple-address-codec")
-const af = require("..").af
+const raf = require('..')
 
 const seed = "ss7vwFuNi2Zud1ekn68LivP2P7UF1"
-const entropy = addressCodec.decodeSeed(seed)
-const privateGenerator = af.createPrivateGenerator(entropy.bytes)
-const publicGenerator = af.createPublicGenerator(privateGenerator)
+const entropy = raf.address.decodeSecret(seed)
+const privateGenerator = raf.af.createPrivateGenerator(entropy.bytes)
+const publicGenerator = raf.af.createPublicGenerator(privateGenerator)
 console.log(privateGenerator.toString('hex'))
 console.log(publicGenerator.toString('hex'))
 
-const privatekey = af.derivePrivateKey(privateGenerator, 0)
-const publickey1 = af.privateKeyToPublicKey(privatekey)
-const publickey2 = af.derivePublicKey(publicGenerator, 0)
+const privatekey = raf.af.derivePrivateKey(privateGenerator, 0)
+const publickey1 = raf.af.privateKeyToPublicKey(privatekey)
+const publickey2 = raf.af.derivePublicKey(publicGenerator, 0)
 
 console.log('00' + privatekey.toString('hex').toUpperCase())
 console.log(publickey1.toString('hex').toUpperCase())
 console.log(publickey2.toString('hex').toUpperCase())
 
-var createHash = require('create-hash')
-function sha256 (buffer) {
-  return createHash('sha256').update(buffer).digest()
-}
-function ripemd160 (buffer) {
-  return createHash('rmd160').update(buffer).digest()
-}
-
-function hash160 (buffer) {
-  return ripemd160(sha256(buffer))
-}
-
-const address = addressCodec.encodeAccountID(hash160(publickey1))
+const address = raf.address.encodeAddress(publickey1)
 console.log(address)
