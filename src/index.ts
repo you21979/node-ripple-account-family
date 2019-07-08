772d29fe434ef852cad455405e7d9ead3586fb7f
yuki akiyama <you2197901@gmail.com>
2019-07-08 17:55:02 +0900

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as af from './lib/accountfamily'
+import * as address from './lib/misc/address'
 import {RootWallet} from './lib/wallet/rootwallet'
 import {ColdWallet} from './lib/wallet/coldwallet'
 export {
-  af, RootWallet, ColdWallet
+  af, RootWallet, ColdWallet, address
 }
